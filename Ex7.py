e295b728235642f83bf5bc080eae7677b3cd139f
Daniel Lesperance <danlesperance12@gmail.com>
2017-10-11 20:10:57 -0400

    Update Ex7.py

--- Ex7.py
+++ Ex7.py
@@ -38,18 +38,21 @@ def readcodons(codonfile):
     return codons,init
 
 # Determine amino-acid translation based on codon table in "codons".
-# Any codon missing from the table (due to N's or whatever) are
-# translated as X.
+# Any codon missing from the table (due to N's not in third position
+# or whatever) are translated as X.
 def trans(codons,seq,frame):
     aalist = []
     for i in range(0,len(seq),3):
-        tfFix = i + (frame-1)
+        #tfFix will ensure that the codon is read at the right spot
+        tfFix = i + (frame-1) 
         codon = seq[tfFix:tfFix+3]
         aa = codons.get(codon,'X')
         aalist.append(aa)
     aaseq = ''.join(aalist)
     return aaseq
 
+#Create a reverse complement of the original sequence for to account
+#for all 6 translation reading frames (3 forward and 3 reverse)
 def RevComp(seq):
     d = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
     revcomp = []
@@ -59,6 +62,10 @@ def RevComp(seq):
     revcomp = ''.join(revcomp)
     return revcomp
 
+# If N exists in the third position in any of the translation 
+# frames. Ensure the codon file reads the same amino acid for 
+# any letter input, if so fix the sequence to output the correct
+# amino acid sequence. 
 def fixN (codons, seq, frame):
     seq = list(seq)
     for i in range(0, len(seq),3):
@@ -66,20 +73,34 @@ def fixN (codons, seq, frame):
         checkSet = set()
         extraBP = len(seq)%3
         codon = seq[tfFix:tfFix+3]
+        # Potentially easier way to solve the 
+        # out of index error; however, this 
+        # if/else statement worked rather well
         if extraBP == 1:
             f = 1
         elif extraBP == 2:
             f = 2
         else:
             f = 0
+        # ensure the codon will be within the 
+        # index 
         if seq[tfFix] < (len(seq)-f):
+            # Look for an N in the third position
             if seq[tfFix+2] == 'N':
+                # Use a for loop to set 3rd position
+                # to TCA and G respectively
                 for nuc in 'TCAG':
-                    seq[tfFix+2] = nuc
+                    seq[tfFix+2] = nuc 
                     codon = seq[tfFix:tfFix+3]
                     codon = ''.join(codon)
                     aa = codons.get(codon,'X')
                     checkSet.add(aa)
+                # If the codons all produce the same amino
+                # acid, the set's length should only be 1
+                ## At no pt should all of the 4 possiblities
+                ## result in an X, so this solution should work
+                ## a simple check howerver may be to see if X exists
+                ## in the set
                 if len(checkSet) == 1:
                     seq[tfFix+2] = 'T'
                     print "T at index", tfFix+2, "is used in place of the N nucleotide provided"
